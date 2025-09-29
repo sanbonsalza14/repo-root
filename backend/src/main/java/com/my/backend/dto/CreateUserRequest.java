a3d83fe3c33428c6d8eac79abfdb3628c882040c
804-03 <slamguy14@naver.com>
2025-09-29 15:41:10 +0900

    1차 로컬 테스트 완료

---
+++ backend/src/main/java/com/my/backend/dto/CreateUserRequest.java
@@ -0,0 +1,12 @@
+package com.my.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter
+@ToString
+public class CreateUserRequest {
+    private  String name;
+    private String city;
+}
